0c9239dab0fda481577dd1b287312ed29b2021f3
ale51 <naoki.m0513@gmail.com>
2020-06-07 23:12:12 +0900

    delete unused statement

--- src/main/java/com/example/datedemo/LocalDateController.java
+++ src/main/java/com/example/datedemo/LocalDateController.java
@@ -1,7 +1,6 @@
 package com.example.datedemo;
 
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
